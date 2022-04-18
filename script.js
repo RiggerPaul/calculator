e567cc7cdb7bd2a75cee1c3875e8dc1e2083ba01
Paul Alford <paulalford2020@outlook.com>
2022-04-18 16:05:11 +0200

    Initial upload of starter files

---
+++ script.js
@@ -0,0 +1,46 @@
+function add(a, b) {
+	return a + b;
+};
+
+function subtract(a, b) {
+	return a - b;
+};
+	
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b){
+  return a / b;
+};
+
+let c = multiply(3, 2);
+
+/*function sum(array) {
+  return array.reduce((total, current) => total + current, 0);
+
+}*/
+
+/*const power = function(a, b) {
+	return Math.pow(a, b);
+};
+
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+};
+
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};*/
